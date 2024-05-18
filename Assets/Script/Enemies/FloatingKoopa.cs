f79a71f37499be41ea7d8284542920396ac9ebc5
Blinkz-AL <erdem-300@hotmail.com>
2024-05-18 13:39:44 +0300

    Lerp

---
+++ Assets/Script/Enemies/FloatingKoopa.cs
@@ -0,0 +1,99 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FloatingKoopa : MonoBehaviour
+{
+    public Sprite shellSprite;
+    private bool shelled;
+    private bool pushed;
+
+    public float shellSpeed = 12f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!shelled && collision.gameObject.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+
+            if(player.starpower)
+            {
+                Hit();
+            }
+            if(collision.transform.DotTest(transform, Vector2.down))
+            {
+                EnterShell();
+            }else{
+                player.Hit();
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(shelled && other.CompareTag("Player"))
+        {
+            if(!pushed)
+            {
+                Vector2 direction = new Vector2(transform.position.x - other.transform.position.x, 0f);
+                PushShell(direction);
+            }else
+            {
+                Player player = other.GetComponent<Player>();
+                if(player.starpower){
+                    Hit();
+                }else{
+                player.Hit();
+
+                }
+            }
+        }
+        else if(!shelled && other.gameObject.layer == LayerMask.NameToLayer("Shell"))
+        {
+            Hit();
+            
+        }else if(pushed && other.gameObject.CompareTag ("Koopa"))
+        {
+
+            other.GetComponent<FloatingKoopa>().Hit();
+        }
+    }
+    private void EnterShell()
+    {
+        shelled = true;
+        GetComponent<LerpKoopa>().enabled = false;
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<SpriteRenderer>().sprite = shellSprite;
+        GameManager.Instance.AddScore(400);
+    }
+    private void PushShell(Vector2 direction)
+    {
+        pushed = true;
+        GetComponent<Rigidbody2D>().isKinematic = false;
+        EntityMovement movement = GetComponent<EntityMovement>();
+        movement.enabled = true;
+        movement.direction = direction.normalized;
+        movement.speed = shellSpeed;
+        movement.enabled = true;
+        gameObject.layer = LayerMask.NameToLayer("Shell");
+    }
+    public void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<DeathAnimation>().enabled = true;
+        Destroy(gameObject, 3f);
+        GameManager.Instance.AddScore(600);
+    }
+
+    private void DestroyKoopa()
+    {
+        Destroy(gameObject);
+    }
+    private void OnBecameInvisible(){
+        if (pushed)
+            Invoke(nameof(DestroyKoopa), 3.0f);
+
+    }
+    private void OnBecameVisible()
+    {
+        if(pushed)
+        CancelInvoke(nameof(DestroyKoopa));
+    }
+}
---
+++ Assets/Script/Enemies/LerpKoopa.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem.iOS;
+
+public class LerpKoopa : MonoBehaviour
+{
+    public float initialDelay; // Başlangıç gecikme süresi
+    public float moveTime; // Lerp işlemi süresi
+    public Vector3 startPos; // Başlangıç pozisyonu
+    public Vector3 endPos; // Bitiş pozisyonu
+    private float timer = 0f; // Zamanlayıcı
+    private bool movingToEnd = true; // Hareket yönü kontrolü
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        enabled = false;
+    }
+
+
+    private void OnBecameVisible()
+    {
+        enabled = true;
+    }
+    private void OnBecameInvisible()
+    {
+        if(gameObject.layer == LayerMask.NameToLayer("Shell"))
+            return;
+        
+        enabled = false;
+    }
+    private void OnEnable()
+    {
+        rb.WakeUp();
+    }
+    private void OnDisable()
+    {
+        rb.velocity = Vector2.zero;
+        rb.Sleep();
+    }
+    void Update()
+    {
+        // Başlangıç gecikme süresini kontrol et
+        if (initialDelay > 0f)
+        {
+            initialDelay -= Time.deltaTime;
+        }
+        else
+        {
+            // Lerp işlemini başlat
+            timer += Time.deltaTime;
+            float t = Mathf.Clamp01(timer / moveTime);
+            if (movingToEnd)
+            {
+                transform.position = Vector3.Lerp(startPos, endPos, t);
+                if (t >= 1f)
+                {
+                    // Bitiş pozisyonuna ulaşıldığında hareket yönünü değiştir
+                    timer = 0f;
+                    movingToEnd = false;
+                }
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(endPos, startPos, t);
+                if (t >= 1f)
+                {
+                    // Başlangıç pozisyonuna ulaşıldığında hareket yönünü değiştir
+                    timer = 0f;
+                    movingToEnd = true;
+                }
+            }
+        }
+    }
+}
